c83d60f58bd7ca2fd639ae88f234950aa7b924bc
rose_white@taylor.edu <rwhite@cse21812.cs.local>
2021-09-20 21:01:06 -0400

    Completed Milestone 1

--- course/cos232/project/steganography/encoder/encoder.c
+++ course/cos232/project/steganography/encoder/encoder.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(argc, argv)
+        int     argc;
+        char    *argv[];{
+
+    // storing command line arguments
+    const char *inputPath = argv[1];
+    const char *outputPath = argv[2];
+    const char *secretMessage = argv[3];
+
+    printf("Input: %s\n", inputPath);
+    printf("Output: %s\n", outputPath);
+    printf("Message: %s\n", secretMessage);
+
+    int is_ok = EXIT_FAILURE;
+
+    // open bitmap
+    FILE* fp = fopen(inputPath, "r");
+    if(!fp) {
+        perror("The file didn't open");
+        return is_ok;
+    }
+
+    // open storage file
+    FILE* fp2 = fopen(outputPath, "w");
+    if(!fp2) {
+        perror("The file didn't open");
+        return is_ok;
+    }
+
     return 0;
 }
